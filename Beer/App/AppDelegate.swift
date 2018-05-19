5019bc977134ab0b13f7c427fb0879f8f1bf8e03
John Lima <john.limadeveloper@gmail.com>
2018-05-19 11:58:35 -0300

    Start project

---
+++ Beer/App/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Beer
+//
+//  Created by John Lima on 19/05/18.
+//  Copyright © 2018 limadeveloper. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+}
---
+++ Beer/Contents/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  Beer
+//
+//  Created by John Lima on 19/05/18.
+//  Copyright © 2018 limadeveloper. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
