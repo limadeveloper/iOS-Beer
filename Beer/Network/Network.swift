db8aedf8d77616163ef937f21316e004083aaa7d
John Lima <john.limadeveloper@gmail.com>
2018-05-19 17:21:27 -0300

    Add network layer

---
+++ Beer/Network/Network.swift
@@ -0,0 +1,19 @@
+//
+//  Network.swift
+//  Beer
+//
+//  Created by John Lima on 19/05/18.
+//  Copyright © 2018 limadeveloper. All rights reserved.
+//
+
+import Foundation
+
+class Network {
+  
+  // MARK: - Properties
+  let shared = Network()
+  
+  // MARK: - Enums
+  
+  // MARK: - Public Methos
+}
